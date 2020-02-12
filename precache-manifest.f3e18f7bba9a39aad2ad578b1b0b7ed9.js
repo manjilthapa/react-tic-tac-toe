b35595ef81a32f7ae1ecd3928cb74263c378d17a
manjilthapa <manjilthapa@gmail.com>
2020-02-12 22:37:00 +0100

    Updates

---
+++ precache-manifest.f3e18f7bba9a39aad2ad578b1b0b7ed9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b32fd0a2d522381883c620620feef48",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "4fa7d51f19cdc0e8c413",
+    "url": "/tic-tac-toe/static/css/main.e7f3bc8f.chunk.css"
+  },
+  {
+    "revision": "73d80054ac9adfa815b5",
+    "url": "/tic-tac-toe/static/js/2.0b92ea1e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tic-tac-toe/static/js/2.0b92ea1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa7d51f19cdc0e8c413",
+    "url": "/tic-tac-toe/static/js/main.bc7b8033.chunk.js"
+  },
+  {
+    "revision": "8fa27791a4d777e376b0",
+    "url": "/tic-tac-toe/static/js/runtime-main.fa48b87c.js"
+  }
+]);
